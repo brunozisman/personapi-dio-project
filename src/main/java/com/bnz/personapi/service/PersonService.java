feac2d01e0fea2621be0f3b5df2ed2b6e429cd3f
brunozisman <brunonzisman@hotmail.com>
2021-09-05 14:54:13 -0300

    Developed list all method

--- src/main/java/com/bnz/personapi/service/PersonService.java
+++ src/main/java/com/bnz/personapi/service/PersonService.java
@@ -9,6 +9,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
@@ -25,5 +28,13 @@ public class PersonService {
                 .builder()
                 .message("Created person with ID: " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
